f0793442425db9dff8a8b8d0e6557415b975fa96
zeapoz <zeapo@pm.me>
2022-09-19 14:00:37 +0200

    nixos: move home-manager to module

---
+++ nixos/modules/home.nix
@@ -0,0 +1,94 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    <home-manager/nixos>
+  ];
+
+  home-manager.users.jonathan = { pkgs, ... }: {
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
+
+    # Packages that should be installed to the user profile.
+    home.packages = with pkgs; [
+      alacritty
+      bat
+      brightnessctl
+      discord
+      emacs28NativeComp
+      exa
+      fd
+      freetube
+      grim
+      htop
+      killall
+      neofetch
+      nixfmt
+      nodePackages.npm
+      pavucontrol
+      pulseaudio
+      python3
+      qutebrowser
+      ripgrep
+      river
+      rivercarro
+      rustup
+      slurp
+      spotify
+      swaybg
+      swayidle
+      swaylock
+      wl-clipboard
+    ];
+
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    home.stateVersion = "22.05";
+
+    # Let Home Manager install and manage itself.
+    programs.home-manager.enable = true;
+
+    programs.fish = {
+      enable = true;
+      shellAliases = {
+        ls = "exa -1 --group-directories-first --icons";
+      };
+    };
+
+    programs.starship = {
+      enable = true;
+      settings = {
+        add_newline = true;
+        character = {
+          success_symbol = "➜";
+          error_symbol = "➜";
+        };
+      };
+    };
+
+    programs.git = {
+      enable = true;
+      userEmail = "zeapo@pm.me";
+      userName = "zeapoz";
+    };
+
+    programs.rofi = {
+      enable = true;
+      package = pkgs.rofi-wayland;
+      theme = "gruvbox-dark-soft";
+      font = "Fira Code 14";
+      extraConfig = {
+        show-icons = true;
+      };
+    };
+
+    programs.waybar.enable = true; # TODO: Move config file here.
+    programs.zoxide.enable = true;
+  };
+}
