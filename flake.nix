2f74ee30ef9907f67b4a34824cffa64026e1b8d7
zeapoz <zeapo@pm.me>
2022-09-19 22:25:00 +0200

    nixos: init flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "My splendid NixOS configuration.";
+
+  inputs = {
+    nixpks.url = "nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+
+      lib = nixpkgs.lib;
+    in {
+      nixosConfigurations = {
+        nixos = lib.nixosSystem {
+          inherit system;
+
+          modules = [
+            ./configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.jonathan = import ./home.nix;
+            }
+          ];
+        };
+      };
+    };
+}
