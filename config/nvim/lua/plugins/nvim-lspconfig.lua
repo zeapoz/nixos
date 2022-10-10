73c10024956dd54445f59c2098fde4626293a426
zeapoz <zeapo@pm.me>
2022-10-10 16:52:06 +0200

    nvim: which key legends

--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,6 +1,6 @@
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
@@ -14,7 +14,7 @@ local on_attach = function(client, bufnr)
 
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
@@ -36,16 +36,23 @@ local lsp = require('lspconfig')
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 lsp['rnix'].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
+  on_attach = on_attach,
+  capabilities = capabilities,
 }
 
 lsp['sumneko_lua'].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
+      }
+    }
+  }
 }
 
 lsp['rust_analyzer'].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
+  on_attach = on_attach,
+  capabilities = capabilities,
 }
--- config/nvim/lua/plugins/which-key-nvim.lua
+++ config/nvim/lua/plugins/which-key-nvim.lua
@@ -1,6 +1,19 @@
 local wk = require('which-key')
 
 wk.register({
+  b = {
+    name = 'Buffers',
+    d = { ':bp<bar>bd#<CR>', 'Delete Buffer' },
+    k = { ':bd<CR>', 'Kill Buffer' },
+  },
+  c = {
+    name = 'LSP',
+    a = 'Code Actions',
+    f = 'Format Code',
+  },
+  d = {
+    name = 'Type Definition',
+  },
   f = {
     name = 'Telescope',
     f = { ':Telescope find_files<CR>', 'Find File' },
@@ -16,6 +29,16 @@ wk.register({
     f = { ':NvimTreeFindFile<CR>', 'Find File NvimTree' },
     t = { ':ToggleTerm<CR>', 'Toggle Terminal' },
     h = { ':Dashboard<CR>', 'Dashboard' },
-    e = { ':DashboardNewFile<CR>', 'Empty buffer' },
+    e = { ':DashboardNewFile<CR>', 'Empty Buffer' },
+  },
+  r = {
+    name = 'Rename',
+    n = 'LSP Rename',
+  },
+  w = {
+    name = 'Workspaces',
+    a = { 'Add Workspace Folder' },
+    l = { 'List Workspace Folders' },
+    r = { 'Remove Workspace Folder' },
   },
 }, { prefix = '<space>' })
