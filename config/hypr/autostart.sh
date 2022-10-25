c25ee85f089e91f3e5f857d2cd6bda584819eade
zeapoz <zeapo@pm.me>
2022-10-25 18:16:54 +0200

    wm: remove background services

--- config/hypr/autostart.sh
+++ config/hypr/autostart.sh
@@ -7,6 +7,3 @@ killall -q swaybg -9
 # Start background applications.
 waybar &
 swaybg -i $(find ~/Pictures/Wallpapers -type f | shuf -n 1) -m fill &
-
-# Start background services.
-emacs --daemon &
