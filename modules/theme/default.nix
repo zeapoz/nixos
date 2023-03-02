4c21171ea51cbfd61f7758268967674c07a572b7
zeapoz <zeapo@pm.me>
2023-03-02 21:31:00 +0100

    theme: style waybar + add doom-one

--- modules/theme/default.nix
+++ modules/theme/default.nix
@@ -3,6 +3,7 @@ with lib;
 let
   cfg = config.modules.theme;
   nix-colors = inputs.nix-colors;
+  colors = config.colorScheme.colors;
 in
 {
   imports = [ nix-colors.homeManagerModule ];
@@ -10,53 +11,71 @@ in
   options.modules.theme.enable = mkEnableOption "theme";
 
   config = mkIf cfg.enable {
-    colorScheme = nix-colors.colorSchemes.nord;
+    colorScheme = import ./doom-one.nix;
 
-    hm.user = {
-      programs.kitty.settings = {
-        foreground = "#${config.colorScheme.colors.base05}";
-        background = "#${config.colorScheme.colors.base00}";
-        color0 = "#${config.colorScheme.colors.base01}"; # Black
-        color1 = "#${config.colorScheme.colors.base08}"; # Red
-        color2 = "#${config.colorScheme.colors.base0B}"; # Green
-        color3 = "#${config.colorScheme.colors.base0A}"; # Yellow
-        color4 = "#${config.colorScheme.colors.base0D}"; # Blue
-        color5 = "#${config.colorScheme.colors.base0E}"; # Magenta
-        color6 = "#${config.colorScheme.colors.base0C}"; # Cyan
-        color7 = "#${config.colorScheme.colors.base04}"; # White
-        color8 = "#${config.colorScheme.colors.base01}"; # Black
-        color9 = "#${config.colorScheme.colors.base08}"; # Red
-        color10 = "#${config.colorScheme.colors.base0B}"; # Green
-        color11 = "#${config.colorScheme.colors.base0A}"; # Yellow
-        color12 = "#${config.colorScheme.colors.base0D}"; # Blue
-        color13 = "#${config.colorScheme.colors.base0E}"; # Magenta
-        color14 = "#${config.colorScheme.colors.base0C}"; # Cyan
-        color15 = "#${config.colorScheme.colors.base04}"; # White
+    hm = {
+      user = {
+        programs.kitty.settings = with colors; {
+          foreground = "#${fg}";
+          background = "#${bg}";
+          color0 = "#${black}";
+          color1 = "#${red}";
+          color2 = "#${green}";
+          color3 = "#${yellow}";
+          color4 = "#${blue}";
+          color5 = "#${purple}";
+          color6 = "#${cyan}";
+          color7 = "#${white}";
+          color8 = "#${lightBlack}";
+          color9 = "#${lightRed}";
+          color10 = "#${lightGreen}";
+          color11 = "#${lightYellow}";
+          color12 = "#${lightBlue}";
+          color13 = "#${lightPurple}";
+          color14 = "#${lightCyan}";
+          color15 = "#${gray}";
+        };
+
+        home.pointerCursor = {
+          name = "Bibata-Original-Classic";
+          package = pkgs.bibata-cursors;
+          size = 24;
+          gtk.enable = true;
+          x11.enable = true;
+        };
+
+        gtk = {
+          enable = true;
+          font = {
+            name = "Fira Sans";
+            size = 12;
+          };
+          iconTheme = {
+            name = "Adwaita";
+            package = pkgs.gnome.adwaita-icon-theme;
+          };
+          theme = {
+            name = "adw-gtk3-dark";
+            package = pkgs.adw-gtk3;
+          };
+        };
       };
 
-      home.pointerCursor = {
-        name = "Bibata-Original-Classic";
-        package = pkgs.bibata-cursors;
-        size = 24;
-        gtk.enable = true;
-        x11.enable = true;
-      };
-
-      gtk = {
-        enable = true;
-        font = {
-          name = "Fira Sans";
-          size = 12;
-        };
-        iconTheme = {
-          name = "Adwaita";
-          package = pkgs.gnome.adwaita-icon-theme;
-        };
-        theme = {
-          name = "adw-gtk3-dark";
-          package = pkgs.adw-gtk3;
-        };
-      };
+      configFile."waybar/colors.css".text = with colors; ''
+        @define-color bg #${bg};
+        @define-color fg #${fg};
+        @define-color red-normal #${red};
+        @define-color red-accent #${lightRed};
+        @define-color green-normal #${green};
+        @define-color green-accent #${lightGreen};
+        @define-color yellow-normal #${yellow};
+        @define-color yellow-accent #${lightYellow};
+        @define-color blue-normal #${blue};
+        @define-color blue-accent #${lightBlue};
+        @define-color purple-normal #${purple};
+        @define-color purple-accent #${lightPurple};
+      '';
     };
   };
 }
+
---
+++ modules/theme/doom-one.nix
@@ -0,0 +1,24 @@
+{
+  name = "Doom One";
+  author = "Henrik Lissner (https://github.com/hlissner)";
+  colors = {
+    fg = "#bbc2cf";
+    bg = "#282c34";
+    black = "#2a2e38";
+    red = "#ff6c6b";
+    green = "#98be65";
+    yellow = "#ECBE7B";
+    blue = "#51afef";
+    purple = "#c678dd";
+    cyan = "#46D9FF";
+    white = "#DFDFDF";
+    lightBlack = "#3f444a";
+    lightRed = "#ff6655";
+    lightGreen = "#99bb66";
+    lightYellow = "#ECBE7B";
+    lightBlue = "#51afef";
+    lightPurple = "#c678dd";
+    lightCyan = "#46D9FF";
+    gray = "#bbc2cf";
+  };
+}
