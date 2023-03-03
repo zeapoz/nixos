68f15c2fd9d4afcaf4831315150d583173c84634
zeapoz <zeapo@pm.me>
2023-03-03 14:19:44 +0100

    theme: add gruvbox

---
+++ modules/theme/gruvbox.nix
@@ -0,0 +1,24 @@
+{
+  name = "Gruvbox Dark";
+  author = "Pavel Pertsev (https://github.com/morhetz)";
+  colors = {
+    fg = "#ebdbb2";
+    bg = "#282828";
+    black = "#1d2021";
+    red = "#cc241d";
+    green = "#98971a";
+    yellow = "#d79921";
+    blue = "#458588";
+    purple = "#b16286";
+    cyan = "#689d6a";
+    gray = "#a89984";
+    lightBlack = "#928374";
+    lightRed = "#fb4934";
+    lightGreen = "#b8bb26";
+    lightYellow = "#fabd2f";
+    lightBlue = "#83a598";
+    lightPurple = "#d3869b";
+    lightCyan = "#8ec07c";
+    white = "#ebdbb2";
+  };
+}
