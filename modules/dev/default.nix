5a2d43f53b1d62e6a617a8cae2a7fb71d008721a
zeapoz <zeapo@pm.me>
2023-04-08 17:48:38 +0200

    direnv: init

--- modules/dev/default.nix
+++ modules/dev/default.nix
@@ -20,6 +20,11 @@ in {
       ];
 
       programs = {
+        direnv = {
+          enable = true;
+          nix-direnv.enable = true;
+        };
+
         git = {
           enable = true;
           userEmail = "zeapo@pm.me";
