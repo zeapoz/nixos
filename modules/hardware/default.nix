657713f4902d34b64546250c1efccd2ac835cb1c
zeapoz <zeapo@pm.me>
2023-08-17 22:41:54 +0200

    fix: kanata disable-while-typing

--- modules/hardware/default.nix
+++ modules/hardware/default.nix
@@ -4,4 +4,12 @@
   imports = [ ./bluetooth.nix ./keychron.nix ];
 
   hm.packages = with pkgs; [ kanata ];
+
+  # Fix disable while typing when using kanata.
+  environment.etc."libinput/local-overrides.quirks".text = ''
+    [kanata]
+    MatchUdevType=keyboard
+    MatchName=kanata
+    AttrKeyboardIntegration=internal
+  '';
 }
