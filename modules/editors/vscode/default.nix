cb706caebf7d53322f762d47856ad248fe540000
zeapoz <zeapo@pm.me>
2024-05-29 19:37:21 +0200

    vscode: useCodium option

--- modules/editors/vscode/default.nix
+++ modules/editors/vscode/default.nix
@@ -9,28 +9,32 @@ with lib; let
 in {
   options.modules.editors.vscode = {
     enable = mkEnableOption "vscode";
+    useCodium = mkEnableOption "codium";
     configRepo = mkStrOpt "https://github.com/zeapoz/vscode";
   };
 
   config = mkIf cfg.enable {
     hm = {
-      programs.vscode = {
-        enable = true;
-        package = pkgs.vscodium;
-        extensions = with pkgs.vscode-extensions; [
-          serayuzgur.crates
-          mkhl.direnv
-          usernamehw.errorlens
-          tamasfe.even-better-toml
-          pkief.material-icon-theme
-          jnoortheen.nix-ide
-          rust-lang.rust-analyzer
-          gruntfuggly.todo-tree
-          vscodevim.vim
-        ];
-      };
+      programs.vscode =
+        if cfg.useCodium
+        then {
+          enable = true;
+          package = pkgs.vscodium;
+          extensions = with pkgs.vscode-extensions; [
+            serayuzgur.crates
+            mkhl.direnv
+            usernamehw.errorlens
+            tamasfe.even-better-toml
+            pkief.material-icon-theme
+            jnoortheen.nix-ide
+            rust-lang.rust-analyzer
+            gruntfuggly.todo-tree
+            vscodevim.vim
+          ];
+        }
+        else {enable = true;};
 
-      user.home.activation = {
+      user.home.activation = mkIf cfg.useCodium {
         installConfig = ''
           if [ ! -d "$HOME/.config/VSCodium/User" ]; then
              git clone "${cfg.configRepo}" "$HOME/.config/VSCodium/User"
