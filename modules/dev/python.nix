96764ceee88c6520d60e8a8f6673c6e93e0c38ec
zeapoz <zeapo@pm.me>
2024-06-10 21:53:25 +0200

    fix: build

--- modules/dev/python.nix
+++ modules/dev/python.nix
@@ -10,16 +10,13 @@ in {
   options.modules.dev.python.enable = mkBoolOpt true;
 
   config = mkIf cfg.enable {
-    hm.packages = with pkgs;
-      [
-        black
-        ruff
-        ruff-lsp
-        isort
-        python3
-      ]
-      ++ (with nodePackages; [
-        nodePackages.pyright
-      ]);
+    hm.packages = with pkgs; [
+      black
+      ruff
+      ruff-lsp
+      isort
+      python3
+      pyright
+    ];
   };
 }
