0f19a253e4df02a5571fb69e039bffde8b5408e1
zeapoz <zeapo@pm.me>
2024-06-15 16:26:22 +0200

    fix: build

--- modules/dev/shell.nix
+++ modules/dev/shell.nix
@@ -10,10 +10,9 @@ in {
   options.modules.dev.shell.enable = mkBoolOpt true;
 
   config = mkIf cfg.enable {
-    hm.packages = with pkgs;
-      [shfmt]
-      ++ (with nodePackages; [
-        nodePackages.bash-language-server
-      ]);
+    hm.packages = with pkgs; [
+      shfmt
+      bash-language-server
+    ];
   };
 }
