4047c7b7fb1e0b54aabee7929098cb8a5972394c
zeapoz <zeapo@pm.me>
2024-07-27 15:00:28 +0200

    ref: enabled -> type

--- configuration.nix
+++ configuration.nix
@@ -59,7 +59,7 @@
 
   # Japanese IME.
   i18n.inputMethod = {
-    enabled = "fcitx5";
+    type = "fcitx5";
     fcitx5.waylandFrontend = true;
     fcitx5.addons = with pkgs; [fcitx5-mozc fcitx5-gtk];
   };
