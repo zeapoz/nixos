4b6879cfe31fcbae95c05bb0bf9484bafaba5803
zeapoz <zeapo@pm.me>
2025-04-20 09:46:28 +0200

    shell: init fastfetch

--- modules/shell/default.nix
+++ modules/shell/default.nix
@@ -18,7 +18,6 @@ in {
         fzf
         killall
         lazygit
-        neofetch
         ripgrep
         unzip
         zip
@@ -37,6 +36,7 @@ in {
           shellAliases = {
             lg = "lazygit";
             cat = "bat --theme ansi";
+            ff = "fastfetch";
             "..." = "cd ../..";
           };
           shellInit = ''
@@ -70,6 +70,145 @@ in {
           extraOptions = ["--group-directories-first"];
           git = true;
           icons = "auto";
+        };
+
+        fastfetch = {
+          enable = true;
+          settings = {
+            logo = {
+              type = "builtin";
+              height = 15;
+              width = 30;
+              padding = {
+                top = 5;
+                left = 3;
+              };
+            };
+            modules = [
+              "break"
+              {
+                type = "custom";
+                format = "┌──────────────────────Hardware──────────────────────┐";
+                outputColor = "90";
+              }
+              {
+                type = "host";
+                key = " PC";
+                keyColor = "green";
+              }
+              {
+                type = "cpu";
+                key = "│ ├";
+                keyColor = "green";
+              }
+              {
+                type = "gpu";
+                key = "│ ├";
+                keyColor = "green";
+              }
+              {
+                type = "memory";
+                key = "│ ├";
+                keyColor = "green";
+              }
+              {
+                type = "disk";
+                key = "└ └󰨣";
+                keyColor = "green";
+              }
+              {
+                type = "custom";
+                format = "└────────────────────────────────────────────────────┘";
+                outputColor = "90";
+              }
+              "break"
+              {
+                type = "custom";
+                format = "┌──────────────────────Software──────────────────────┐";
+                outputColor = "90";
+              }
+              {
+                type = "os";
+                key = " OS";
+                keyColor = "yellow";
+              }
+              {
+                type = "kernel";
+                key = "│ ├󰌽";
+                keyColor = "yellow";
+              }
+              {
+                type = "bios";
+                key = "│ ├";
+                keyColor = "yellow";
+              }
+              {
+                type = "packages";
+                key = "│ ├󰏓";
+                keyColor = "yellow";
+              }
+              {
+                type = "shell";
+                key = "└ └";
+                keyColor = "yellow";
+              }
+              "break"
+              {
+                type = "wm";
+                key = " DE";
+                keyColor = "blue";
+              }
+              {
+                type = "lm";
+                key = "│ ├󰍂";
+                keyColor = "blue";
+              }
+              {
+                type = "wmtheme";
+                key = "│ ├󰉼";
+                keyColor = "blue";
+              }
+              {
+                type = "terminal";
+                key = "└ └";
+                keyColor = "blue";
+              }
+              {
+                type = "custom";
+                format = "└────────────────────────────────────────────────────┘";
+                outputColor = "90";
+              }
+              "break"
+              {
+                type = "custom";
+                format = "┌─────────────────Uptime / Age / DT─/────────────────┐";
+                outputColor = "90";
+              }
+              {
+                type = "command";
+                key = "  OS Age ";
+                keyColor = "magenta";
+                text = "birth_install=$(stat -c %W /); current=$(date +%s); time_progression=$((current - birth_install)); days_difference=$((time_progression / 86400)); echo $days_difference days";
+              }
+              {
+                type = "uptime";
+                key = "  Uptime ";
+                keyColor = "magenta";
+              }
+              {
+                type = "datetime";
+                key = "  DateTime ";
+                keyColor = "magenta";
+              }
+              {
+                type = "custom";
+                format = "└────────────────────────────────────────────────────┘";
+                outputColor = "90";
+              }
+              "break"
+              "colors"
+            ];
+          };
         };
       };
     };
