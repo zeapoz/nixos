c84dfbe262c0cdd51feffcb0d9453a5527a329af
zeapoz <zeapo@pm.me>
2025-04-27 10:55:59 +0200

    ref: new file structure

--- modules/desktop/applications/default.nix
+++ modules/desktop/applications/default.nix
@@ -1,21 +1,47 @@
-{pkgs, ...}: {
-  hm.packages = with pkgs; [
-    anki
-    aseprite
-    blender
-    bitwarden
-    discord
-    mullvad-vpn
-    obsidian
-    pavucontrol
-    playerctl
-    pulseaudio
-    signal-desktop-bin
-    telegram-desktop
-    slack
-    zathura
-    zoom-us
-  ];
+{
+  pkgs,
+  inputs,
+  ...
+}: let
+  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+in {
+  hm = {
+    user.imports = [
+      inputs.spicetify-nix.homeManagerModules.default
+    ];
+
+    packages = with pkgs; [
+      anki
+      aseprite
+      bitwarden
+      blender
+      brave
+      calibre
+      discord
+      firefox
+      mullvad-vpn
+      musescore
+      obsidian
+      pavucontrol
+      playerctl
+      pulseaudio
+      qbittorrent
+      signal-desktop-bin
+      slack
+      stremio
+      telegram-desktop
+      zathura
+      zoom-us
+      inputs.zen-browser.packages."${system}".twilight
+    ];
+
+    programs = {
+      spicetify = {
+        enable = true;
+        theme = spicePkgs.themes.defaultDynamic;
+      };
+    };
+  };
 
   programs = {
     thunar = {
