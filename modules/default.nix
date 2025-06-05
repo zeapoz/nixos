f6a1fdb11168775aa695fd3768115ea5e1a6bbf6
zeapoz <zeapo@pm.me>
2025-06-05 13:47:53 +0200

    deps: bump

--- modules/default.nix
+++ modules/default.nix
@@ -75,18 +75,18 @@ in {
   time.timeZone = "Europe/Stockholm";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.utf8";
+  i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "sv_SE.utf8";
-    LC_IDENTIFICATION = "sv_SE.utf8";
-    LC_MEASUREMENT = "sv_SE.utf8";
-    LC_MONETARY = "sv_SE.utf8";
-    LC_NAME = "sv_SE.utf8";
-    LC_NUMERIC = "sv_SE.utf8";
-    LC_PAPER = "sv_SE.utf8";
-    LC_TELEPHONE = "sv_SE.utf8";
-    LC_TIME = "en_US.utf8";
+    LC_ADDRESS = "sv_SE.UTF-8";
+    LC_IDENTIFICATION = "sv_SE.UTF-8";
+    LC_MEASUREMENT = "sv_SE.UTF-8";
+    LC_MONETARY = "sv_SE.UTF-8";
+    LC_NAME = "sv_SE.UTF-8";
+    LC_NUMERIC = "sv_SE.UTF-8";
+    LC_PAPER = "sv_SE.UTF-8";
+    LC_TELEPHONE = "sv_SE.UTF-8";
+    LC_TIME = "en_US.UTF-8";
   };
 
   # Japanese IME.
