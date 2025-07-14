de99b61f591faa19f2f2fd3e8452af12e5ea949c
Jonathan Andersson <zeapo@pm.me>
2025-07-14 17:18:05 +0200

    anyrun: style update

--- modules/desktop/applications/anyrun/default.nix
+++ modules/desktop/applications/anyrun/default.nix
@@ -1,15 +1,16 @@
 {
+  config,
   pkgs,
   inputs,
   ...
 }: {
-  hm.programs = {
-    anyrun = {
+  hm = {
+    programs.anyrun = {
       enable = true;
       config = {
         x = {fraction = 0.5;};
         y = {fraction = 0.3;};
-        width = {fraction = 0.3;};
+        width = {fraction = 0.2;};
         hidePluginInfo = true;
         closeOnClick = true;
         showResultsImmediately = true;
@@ -25,51 +26,8 @@
           websearch
         ];
       };
-      extraCss = ''
-        * {
-          all: unset;
-        }
-
-        #window,
-        #match,
-        #entry,
-        #plugin,
-        #main {
-          background: transparent;
-        }
-
-        #match.activatable {
-          border-radius: 10px;
-          margin: 4px 2px;
-          padding: 0 8px;
-        }
-        #match.activatable:first-child {
-          margin-top: 12px;
-        }
-        #match.activatable:last-child {
-          margin-bottom: 0;
-        }
-
-        #match:hover {
-          background: rgba(255, 255, 255, 0.05);
-        }
-        #match:selected {
-          background: rgba(255, 255, 255, 0.1);
-        }
-
-        #entry {
-          background: rgba(255, 255, 255, 0.05);
-          border: 1px solid rgba(255, 255, 255, 0.1);
-          border-radius: 8px;
-          padding: 4px 8px;
-        }
-
-        box#main {
-          background: rgba(0, 0, 0, 0.4);
-          padding: 12px;
-          border-radius: 10px;
-        }
-      '';
     };
+
+    configFile."anyrun/style.css".source = config.lib.meta.mkMutableSymlink ./style.css;
   };
 }
